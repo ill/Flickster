158e1451c49e249286749d005727b32b14221db9
Ilya Seletsky <ilselets@microsoft.com>
2017-09-16 18:58:15 -0700

    Reacting to different orientations.

--- app/src/main/java/com/codepath/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/flickster/adapters/MovieArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.flickster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -65,7 +66,14 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         viewHolder.tvTitle.setText(movie.getOriginalTitle());
         viewHolder.tvOverview.setText(movie.getOverview());
 
-        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImage);
+        int orientation = getContext().getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.ivImage);
+        }
+        else {
+            Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImage);
+        }
 
         return convertView;
     }
